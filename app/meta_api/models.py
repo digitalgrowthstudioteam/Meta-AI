eba9ef95b8774f983fbf26926e25383f6b06ec29
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2025-12-31 09:15:53 +0530

    Update models.py

--- app/meta_api/models.py
+++ app/meta_api/models.py
@@ -1,3 +1,5 @@
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import String, Boolean, DateTime, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
