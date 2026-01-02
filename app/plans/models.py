d11794d9d51c67cdf249bc1b731b11e6e2cca481
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-02 12:38:05 +0530

    Update models.py

--- app/plans/models.py
+++ app/plans/models.py
@@ -10,7 +10,7 @@ from sqlalchemy import (
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
-from app.core.database import Base
+from app.core.base import Base
 
 
 class Plan(Base):
