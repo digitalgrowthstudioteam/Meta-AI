3a06f8d0eef92d2cb4674a02c0d0cef47d5405f9
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-02 12:37:39 +0530

    Update models.py

--- app/users/models.py
+++ app/users/models.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import datetime
 import uuid
 
-from app.core.database import Base
+from app.core.base import Base
 
 
 class User(Base):
