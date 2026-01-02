48f414d1b452188895e8e1e119150747b6c2b26d
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-02 14:35:19 +0530

    Update models.py

--- app/auth/models.py
+++ app/auth/models.py
@@ -12,7 +12,8 @@ from sqlalchemy import (
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
-from app.core.database import Base
+from app.core.base import Base
+
 
 # =========================================================
 # AUTH SESSION MODEL (SERVER-SIDE SESSIONS)
