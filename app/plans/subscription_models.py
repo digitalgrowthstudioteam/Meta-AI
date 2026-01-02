5697c3b1aa352c1efb358a7e8e3247d6e58ed88f
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-02 12:38:29 +0530

    Update subscription_models.py

--- app/plans/subscription_models.py
+++ app/plans/subscription_models.py
@@ -12,7 +12,7 @@ from sqlalchemy import (
 )
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from app.core.database import Base
+from app.core.base import Base
 
 
 class Subscription(Base):
