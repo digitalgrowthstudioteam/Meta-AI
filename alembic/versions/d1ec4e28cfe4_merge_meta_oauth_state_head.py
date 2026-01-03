c690caaeb6e2efe5748cff584445003328e902ae
root <root@srv1228780.hstgr.cloud>
2026-01-03 05:48:58 +0000

    alembic: merge meta oauth state heads

---
+++ alembic/versions/d1ec4e28cfe4_merge_meta_oauth_state_head.py
@@ -0,0 +1,24 @@
+"""merge meta oauth state head
+
+Revision ID: d1ec4e28cfe4
+Revises: 20b76651ed78, 897d3889e5e5
+Create Date: 2026-01-03 05:47:43.044068
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd1ec4e28cfe4'
+down_revision = ('20b76651ed78', '897d3889e5e5')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
