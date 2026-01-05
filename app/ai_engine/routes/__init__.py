02b619e0abae6dd13422a2ead0401abd6559099c
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-05 15:55:09 +0530

    Update __init__.py

--- app/ai_engine/routes/__init__.py
+++ app/ai_engine/routes/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.ai_engine.routes.actions import router as actions_router
 from app.ai_engine.routes.category_insights_routes import router as category_router
+from .audience_insights_routes import router as audience_insights_router
 
 
 router = APIRouter(
