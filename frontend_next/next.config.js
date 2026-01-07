9e34a2051a87abe7056d624ba52796bd8562bc1f
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-07 23:10:36 +0530

    Update next.config.js

--- frontend_next/next.config.js
+++ frontend_next/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`,
+        destination: "https://meta-ai.digitalgrowthstudio.in/api/:path*",
       },
     ];
   },
