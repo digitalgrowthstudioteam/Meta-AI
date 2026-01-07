f1b39d8418167795d2ec5f82fd5319f43cac2459
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-07 23:06:18 +0530

    Update next.config.js

--- frontend_next/next.config.js
+++ frontend_next/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
