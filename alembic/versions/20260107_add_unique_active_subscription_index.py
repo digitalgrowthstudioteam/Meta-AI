8daff4768a3a7c42ac1111e27188e1b59216666c
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-07 19:23:01 +0530

    Update 20260107_add_unique_active_subscription_index.py

--- alembic/versions/20260107_add_unique_active_subscription_index.py
+++ alembic/versions/20260107_add_unique_active_subscription_index.py
@@ -1,17 +1,17 @@
-"""add unique active subscription per user constraint
+"""
+add unique active subscription per user constraint
 
 Revision ID: 20260107_add_unique_active_subscription_index
-Revises: 1da1c96ad146
+Revises: 20260107_fix_missing_columns
 Create Date: 2026-01-07
 """
 
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = "20260107_add_unique_active_subscription_index"
-down_revision = "1da1c96ad146"
+down_revision = "20260107_fix_missing_columns"
 branch_labels = None
 depends_on = None
 
