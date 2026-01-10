b0da0a62ec0c335de0dde3ad5955509cedd55a6a
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-10 18:02:06 +0530

    Update next.config.js

--- frontend_next/next.config.js
+++ frontend_next/next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  poweredByHeader: false,
-
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://meta-ai.digitalgrowthstudio.in/api/:path*",
-      },
-    ];
+  reactStrictMode: false,
+  swcMinify: false,
+  webpack: (config) => {
+    return config;
   },
 };
 
