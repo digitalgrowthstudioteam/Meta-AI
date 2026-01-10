212bcdca1155ed19babdd34116fbc6ed86902e10
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-10 20:44:00 +0530

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,6 +3,10 @@
 
 from app.users.models import User
 
+# Payment must be imported BEFORE Subscriptions because Subscriptions link to Payments
+from app.billing.payment_models import Payment
+from app.billing.invoice_models import Invoice
+
 from app.plans.models import Plan
 from app.plans.subscription_models import Subscription
 
@@ -12,6 +16,3 @@ from app.meta_api.models import MetaAdAccount, UserMetaAdAccount, MetaOAuthToken
 from app.campaigns.models import Campaign
 
 from app.ai_engine.models import AIAction
-
-from app.billing.payment_models import Payment
-from app.billing.invoice_models import Invoice
