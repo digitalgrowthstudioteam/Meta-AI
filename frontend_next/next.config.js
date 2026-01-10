582bb442447e0cda050cc0943ebb672bc7f2bf05
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-10 18:57:43 +0530

    Update next.config.js

--- frontend_next/next.config.js
+++ frontend_next/next.config.js
@@ -1,7 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  swcMinify: false,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.fbcdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "**.facebook.com",
+      },
+      {
+        protocol: "https",
+        hostname: "platform-lookaside.fbsbx.com",
+      },
+    ],
+  },
+  // Ensure we can iterate fast without strict build blocking
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     return config;
   },
