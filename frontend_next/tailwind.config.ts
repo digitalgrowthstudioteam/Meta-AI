27a3d42e22d7b0d3ee73ec1872804feae903b1d7
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-10 16:45:50 +0530

    Update tailwind.config.ts

--- frontend_next/tailwind.config.ts
+++ frontend_next/tailwind.config.ts
@@ -2,7 +2,13 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./hooks/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
+    "./**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
