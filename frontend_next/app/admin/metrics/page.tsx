b348fbc859f13048e85615416cb6adf9c86f7509
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-14 13:31:31 +0530

    Create page.tsx

---
+++ frontend_next/app/admin/metrics/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+import useSWR from 'swr';
+import { fetcher } from "../../../lib/fetcher";
+
+export default function AdminMetricsPage() {
+  const { data } = useSWR('/api/admin/metrics/sync-status', fetcher);
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Metrics Sync Status</h1>
+      <div className="bg-white p-6 rounded border shadow-sm">
+         <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    </div>
+  );
+}
