d46b363a8a1e5d8abf488bd6efad65ef9dd92518
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-18 13:31:44 +0530

    Delete frontend_next/app/(app) directory

--- frontend_next/app/(app)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import UserShell from "../UserShell";
-
-export default function AppLayout({ children }: { children: ReactNode }) {
-  return <UserShell>{children}</UserShell>;
-}
