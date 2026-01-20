7376fe21fbefc7a89c5fd9bc2720ec20cc1a568b
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-20 15:53:50 +0530

    Create __init__.py

---
+++ app/admin/routes/__init__.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter
+
+# Import sub-routers
+from .dashboard_routes import router as dashboard_router
+from .user_routes import router as user_router
+from .billing_routes import router as billing_router
+from .risk_routes import router as risk_router
+from .pricing_routes import router as pricing_router
+from .meta_routes import router as meta_router
+from .audit_routes import router as audit_router
+
+# Main combined router for /admin
+router = APIRouter(prefix="/admin", tags=["Admin"])
+
+# Attach child routers
+router.include_router(dashboard_router)
+router.include_router(user_router)
+router.include_router(billing_router)
+router.include_router(risk_router)
+router.include_router(pricing_router)
+router.include_router(meta_router)
+router.include_router(audit_router)
