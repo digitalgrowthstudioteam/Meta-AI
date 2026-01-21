2d6f286c9dab0226689b2ea4e1af42c7c32194f9
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-21 23:43:23 +0530

    Update page.tsx

--- frontend_next/app/checkout/plan/[id]/page.tsx
+++ frontend_next/app/checkout/plan/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { apiFetch } from "@/lib/fetcher";
+import { apiFetch } from "@../../lib/fetcher";
 
 type PublicPlan = {
   id: number;
