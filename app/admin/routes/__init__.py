e29737a9fd52d489e90c94223e53169a7eaeebd6
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-22 19:38:28 +0530

    Update __init__.py

--- app/admin/routes/__init__.py
+++ app/admin/routes/__init__.py
@@ -3,21 +3,21 @@ from fastapi import APIRouter
 from .dashboard_routes import router as dashboard_router
 from .user_routes import router as user_router
 from .billing_admin_routes import router as billing_router
+from .company_billing_routes import router as company_billing_router
 from .plan_routes import router as plan_router
 from .risk_routes import router as risk_router
 from .pricing_routes import router as pricing_router
 from .meta_routes import router as meta_router
 from .audit_routes import router as audit_router
-from .company_billing_routes import router as company_billing_router
 
 router = APIRouter(prefix="/admin", tags=["Admin"])
 
 router.include_router(dashboard_router)
 router.include_router(user_router)
 router.include_router(billing_router)
+router.include_router(company_billing_router)
 router.include_router(plan_router)       # 👈 now officially placed & active
 router.include_router(pricing_router)
 router.include_router(risk_router)
 router.include_router(meta_router)
 router.include_router(audit_router)
-router.include_router(company_billing_router)
