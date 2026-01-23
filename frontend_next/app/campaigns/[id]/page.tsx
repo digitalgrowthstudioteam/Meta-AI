f2dc71a814685d65820ad4f35d10ff6a70c94f12
digitalgrowthstudioteam <digitalgrowthstudioteam@gmail.com>
2026-01-23 22:48:56 +0530

    Update page.tsx

--- frontend_next/app/campaigns/[id]/page.tsx
+++ frontend_next/app/campaigns/[id]/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import toast from "react-hot-toast";
-import { apiFetch } from "../../lib/fetcher";
+import { apiFetch } from "@/app/lib/fetcher";
 
 /* ----------------------------------
  * TYPES
